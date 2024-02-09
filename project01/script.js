4522246c5e7e7b47b5b2d5b08f781131ec3a4845
Lucas Lopes <lukinhaslopes2002@gmail.com>
2024-02-09 15:57:14 -0300

    - updated

--- project01/script.js
+++ project01/script.js
@@ -1,3 +1,28 @@
-console.log("Moha from New Delhi, India.")
-console.log("Meu nome é Magrão!")
-console.log("O meu nome é Negrola.")
+// variables
+let cont = 0
+
+const increaseButton = window.document.getElementById('increase')
+const decreaseButton = window.document.getElementById('decrease')
+const display = window.document.getElementById('counterNum')
+const clearThat = window.document.getElementById('clear')
+
+// functions
+
+function increase() {
+    cont += 1
+    display.innerHTML = cont
+    console.log("Added!")
+}
+
+function decrease() {
+    cont -= 1
+    display.innerHTML = cont
+    console.log("Subtracted!")
+}
+
+function cleanIt() {
+    cont = 0
+    display.innerHTML = cont
+    console.log("Resetted!")
+}
+
